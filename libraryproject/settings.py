482853fd99b5087de0a65bd86d9cf2feb9805f14
Toqa <toqqa1423@gmail.com>
2025-03-22 03:30:27 +0300

    lab_3

--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -53,11 +53,16 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'libraryproject.urls'
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = os.path.join(BASE_DIR, "apps" + os.sep + "templates")
+
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- libraryproject/urls.py
+++ libraryproject/urls.py
@@ -17,6 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+# new *
+import apps.bookmodule.views 
+
+from django.contrib import admin 
+from django.urls import include, path 
+urlpatterns = [ 
+path('admin/', admin.site.urls), 
+path('books/', include("apps.bookmodule.urls")), #include urls.py of bookmodule app 
+path('users/', include("apps.usermodule.urls"))  #include urls.py of usermodule app 
 ]
