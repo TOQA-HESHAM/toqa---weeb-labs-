79cb3f1818b2e68e349f5b8f688ae626a7f6bc43
Toqa <toqqa1423@gmail.com>
2025-03-24 03:18:36 +0300

    lab4

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -6,3 +6,10 @@ urlpatterns = [
     path('<int:bookId>/', views.viewbook),
 
 ]
+
+urlpatterns = [ 
+path('', views.index, name= "books.index"), 
+path('list_books/', views.list_books, name= "books.list_books"), 
+path('<int:bookId>/', views.viewbook, name="books.view_one_book"), 
+path('aboutus/', views.aboutus, name="books.aboutus"), 
+]
